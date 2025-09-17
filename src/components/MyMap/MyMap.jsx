72d9c402c78a5ae90f55a588469afce7e1354059
eminexo20 <eminexoazeri@gmail.com>
2025-09-18 01:43:47 +0400

    Finish

--- src/components/MyMap/MyMap.jsx
+++ src/components/MyMap/MyMap.jsx
@@ -1,48 +1,20 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import React from 'react';
 
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-}
+const MyMap = () => {
+  return (
+    <div style={{ width: '100%', height: '400px', borderRadius: '12px', overflow: 'hidden' }}>
+      <iframe
+        title="Google Map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.089081830971!2d46.5705!3d40.4058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x403f5d1c5b5f5f5f%3A0x123456789abcdef!2sG%C3%B6yg%C3%B6l!5e0!3m2!1sen!2saz!4v1690000000000"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};
 
-const center = {
-  lat: 40.4058, // Göygöl koordinatları
-  lng: 46.5735
-}
-
-function Mymap() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: 'SƏNİN_API_AÇARIN' // Buraya öz Google Maps API açarını yaz
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback((map) => {
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(() => {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={14}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Marker əlavə olundu */}
-      <Marker position={center} />
-    </GoogleMap>
-  ) : (
-    <div>Xəritə yüklənir...</div>
-  )
-}
-
-export default React.memo(Mymap)
+export default MyMap;
